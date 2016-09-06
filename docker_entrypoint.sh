9c28a3f7c0ac5bc4a1a668a54f97847568217cc7
ksunny <ksunny@adobe.com>
2016-09-06 16:47:02 +0530

    fixing /var/run/docker.sock permissions and changing the unit file to pick the latest conatiner

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -2,10 +2,10 @@
 set -e
 
 if [ "$1" = "pumba" ]; then
-  if [ -S /var/run/docker.sock ]; then
-    chown -R core:core /var/run/docker.sock
-  fi
-  exec gosu core:core "$@"
+#  if [ -S /var/run/docker.sock ]; then
+#    chown -R root:root /var/run/docker.sock
+#  fi
+  exec gosu root:root "$@"
 fi
 
 exec "$@"
